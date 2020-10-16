24df7420fdd18d3054bcaa7d24105340b08b0267
Weverson Diazz <wevers.info@gmail.com>
2020-10-16 00:50:10 -0300

    configuração do server
    configuração e instalação de dependências.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const path = require("path");
+const pages = require("./pages");
 
 const server = express();
 
-server.get("/", function(req, res){
-    return res.send("<h1>Riverdale</h1>");
-});
+server.use(express.static("public"));
+
+server.set("views", path.join(__dirname, "views"));
+server.set("view engine", "hbs");
+
+server.get("/", pages.index );
+server.get("/orphanage", pages.orphanage );
+server.get("/orphanages", pages.orphanages );
+server.get("/create-orphanage", pages.createOrphanage );
 
 
 server.listen(5000, function() {
