139d1925e48d50b469b230702db8024c6a6711ae
Weverson Diazz <wevers.info@gmail.com>
2020-10-17 18:39:43 -0300

    Update test.js
    arquivo de teste dos comando do banco de dados

--- src/database/test.js
+++ src/database/test.js
@@ -4,7 +4,7 @@ const saveOrphanage = require("./saveOrphanage");
 Database.then(async function(db) {
 
     // inserir dados na tabela
-    /*await db.run(`
+    await db.run(`
         INSERT INTO orphanages (
             lat,
             lng,
@@ -26,28 +26,28 @@ Database.then(async function(db) {
             "Horário de visitas Das 8h até 18h",
             "0"
         );
-    `);*/
+    `);
 
 
-    // await saveOrphanage(db, {
+    await saveOrphanage(db, {
         
-    //     lat: "-23.614053",
-    //     lng: "-47.0167853",
-    //     name: "Lar dos meninos",
-    //     about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
-    //     whatsapp: "979533-068415",
-    //     images:[
+        lat: "-23.614053",
+        lng: "-47.0167853",
+        name: "Lar dos meninos",
+        about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        whatsapp: "979533-068415",
+        images:[
 
-    //         "https://images.unsplash.com/photo-1540479859555-17af45c78602?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+            "https://images.unsplash.com/photo-1540479859555-17af45c78602?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
 
-    //         "https://images.unsplash.com/photo-1531325082793-ca7c9db6a4c1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+            "https://images.unsplash.com/photo-1531325082793-ca7c9db6a4c1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
 
-    //         "https://images.unsplash.com/photo-1555791019-72d3af01da82?ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80"
-    //     ].toString(),
-    //     instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
-    //     opening_hours: "Horário de visitas Das 8h até 18h",
-    //     open_on_weekends: "0"
-    // });
+            "https://images.unsplash.com/photo-1555791019-72d3af01da82?ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80"
+        ].toString(),
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 8h até 18h",
+        open_on_weekends: "0"
+    });
 
     // consultar dados da tabela
     const selectedOrphanages = await db.all("SELECT * FROM  orphanages");
@@ -55,13 +55,13 @@ Database.then(async function(db) {
 
 
     // consultar somente 1 dado pelo ID
-    // const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3" ');
-    // console.log(orphanage);
+    const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3" ');
+    console.log(orphanage);
 
 
     // Deletar dado da tabela
-    // await db.run("DELETE FROM orphanages WHERE id = '3' ");
+    await db.run("DELETE FROM orphanages WHERE id = '3' ");
 
-    // Começar aula em 57:00
+    
 
 });
