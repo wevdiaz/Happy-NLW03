42f30de5de2dc2c3d2898115b8204542c7b19ca5
Weverson Diazz <wevers.info@gmail.com>
2020-10-18 22:10:53 -0300

    Update page-orphanage.js
    Arrumando a localização do mapa, deixando mais fluído.

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -6,8 +6,12 @@ const options = {
     zoomControl: false
 }
 
+// get values from HTML
+const lat = document.querySelector("span[data-lat]").dataset.lat;
+const lng = document.querySelector("span[data-lng]").dataset.lng;
+
 // Create map 
-const map = L.map('mapid', options ).setView([-23.6060704,-47.0277716], 15);
+const map = L.map('mapid', options ).setView([lat, lng], 15);
 
 //create and add tileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
@@ -22,7 +26,8 @@ const icon = L.icon({
 
 
 // create and add marker
-L.marker([-23.6060704,-47.0277716], { icon }).addTo(map)
+
+L.marker([lat, lng], { icon }).addTo(map)
     
 
 
