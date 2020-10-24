11b6d993b3804200b809afe598214c6155a83346
Weverson Diazz <wevers.info@gmail.com>
2020-10-24 17:19:07 -0300

    Update server.js
    Rota para deletar

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
 const pages = require("./pages");
+const methodOverride = require("method-override");
 
 const server = express();
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static("public"));
+server.use(methodOverride("_method"));
 
 server.set("views", path.join(__dirname, "views"));
 server.set("view engine", "hbs");
@@ -16,7 +18,7 @@ server.get("/orphanage", pages.orphanage );
 server.get("/orphanages", pages.orphanages );
 server.get("/create-orphanage", pages.createOrphanage );
 server.post("/save-orphanage", pages.saveOrphanage );
-server.post("/list-orphanages", pages.deleteOrphanage );
+server.delete("/list-orphanages", pages.deleteOrphanage );
 
 
 server.listen(5000, function() {
