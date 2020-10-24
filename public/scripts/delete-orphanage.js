e3bc54ce450cd068ac909d61c0ca56c084dd9156
Weverson Diazz <wevers.info@gmail.com>
2020-10-24 17:17:39 -0300

    Update delete-orphanage.js
    Pegando o id que será deletado

--- public/scripts/delete-orphanage.js
+++ public/scripts/delete-orphanage.js
@@ -1,38 +1,21 @@
 
-const cards = document.querySelectorAll(".list-orphanages .orphanage-card button");
 const formDelete = document.querySelector(".list-orphanages #formDelete");
-
-
-// for (card of cards) {
-//     const containerCard = card.parentNode;
-//     const cardid = containerCard.querySelector(".info-orphanage input").value;
-
-//     card.addEventListener("submit", function(event){
-
-//         const confirmation = confirm("Deseja apagar?");
-
-//         if (!confirmation) {
-//             console.log("continuar")
-//             event.preventDeafault();
-//             return
-//         }                
-
-        
-        
-//     });
-// }
+let inputFormID = document.querySelector(".list-orphanages form input[type='hidden']");
 
 
 formDelete.addEventListener("submit", function(event){
 
-    const confirmation = confirm("Deseja apagar?");
+    const confirmation = confirm("Deseja apagar?");    
 
-    if (!confirmation) {        
+    const buttonID = event.submitter.getAttribute("id");
+    inputFormID.value = buttonID;
+    
+
+    if (!confirmation) {          
+        
         event.preventDefault();
         return
-    }                
-
-    
+    }    
     
 });
 
